bb1ac30c6fcbc6b6056ecb4fa3608d86f2ff1a75
Jefferyjasmin <71089889+Jefferyjasmin@users.noreply.github.com>
2021-04-16 12:36:00 -0400

    Ecom

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,36 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line max-len
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { config } = require("firebase-functions");
+const { RouterSharp } = require("@material-ui/icons");
+// eslint-disable-next-line max-len
+const stripe = require("stripe")(
+  "sk_test_51IgkSECL4dLf4dW88bNCS6U3Ce1H3lRRinUnWd4crrSJzO57vxgDeegesbOJfuj1PUmjJ6urM69LQOgBZJRnTBuR00qclDzYSR"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// eslint-disable-next-line no-trailing-spaces
+// for api
+const app = express();
+
+// app config
+
+// middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+// api routes
+
+app.get("/", (request, response) => response.status(200).send("hello world"));
+// localhost:5001/ecommerce-c7cd1/us-central1/api
+
+app.post("/payments/create", async (request, response) => {});
+
+// listen command
+
+exports.api = functions.https.onRequest(app);
+
+// https://www.youtube.com/watch?v=RDV3Z1KCBvo&t=25099s
+// Error on this page need help
